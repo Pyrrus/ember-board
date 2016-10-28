9082801a1439f904af4827437ff59c53a577676c
Adam Gorbahn <adamgorbahn@hotmail.com>
2016-10-27 19:25:51 -0700

    download all the files for ember and make some route

---
+++ config/environment.js
@@ -0,0 +1,51 @@
+/* jshint node: true */
+
+module.exports = function(environment) {
+  var ENV = {
+    modulePrefix: 'blog',
+    environment: environment,
+    rootURL: '/',
+    locationType: 'auto',
+    EmberENV: {
+      FEATURES: {
+        // Here you can enable experimental features on an ember canary build
+        // e.g. 'with-controller': true
+      }
+    },
+    firebase: {
+      apiKey: "AIzaSyAqqNtinSa3QV1xTmmhcRa5dmqGuBBmqkY",
+      authDomain: "message-board-8a003.firebaseapp.com",
+      databaseURL: "https://message-board-8a003.firebaseio.com",
+      storageBucket: "message-board-8a003.appspot.com",
+    },
+    APP: {
+      // Here you can pass flags/options to your application instance
+      // when it is created
+    }
+  };
+
+  if (environment === 'development') {
+    // ENV.APP.LOG_RESOLVER = true;
+    // ENV.APP.LOG_ACTIVE_GENERATION = true;
+    // ENV.APP.LOG_TRANSITIONS = true;
+    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
+    // ENV.APP.LOG_VIEW_LOOKUPS = true;
+  }
+
+  if (environment === 'test') {
+    // Testem prefers this...
+    ENV.locationType = 'none';
+
+    // keep test console output quieter
+    ENV.APP.LOG_ACTIVE_GENERATION = false;
+    ENV.APP.LOG_VIEW_LOOKUPS = false;
+
+    ENV.APP.rootElement = '#ember-testing';
+  }
+
+  if (environment === 'production') {
+
+  }
+
+  return ENV;
+};
