49d5efef3d67167dd913c28fb65ab35b5705b0fd
Adam Gorbahn <adamgorbahn@hotmail.com>
2016-10-28 08:44:17 -0700

    edit the question working on app and delete work as will.

--- app/routes/edit.js
+++ app/routes/edit.js
@@ -1,4 +1,30 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model(params) {
+    return this.store.findRecord('question', params.question_id);
+  },
+  actions: {
+    update(question) {
+      var params = {
+        question: this.get('question'),
+        author: this.get('author'),
+        content: this.get('content'),
+      };
+
+      Object.keys(params).forEach(function(key) {
+        if(params[key]!==undefined) {
+          question.set(key,params[key]);
+        }
+      });
+      question.save();
+      this.transitionTo('/view/' + question.id);
+    }
+  },
+  beforeModel: function(){
+    this._super(...arguments);
+    if (typeof tinymce === 'undefined'){
+      return Ember.$.getScript('//cdn.tinymce.com/4/tinymce.min.js');
+    }
+  }
 });
--- app/routes/questions.js
+++ app/routes/questions.js
@@ -4,4 +4,16 @@ export default Ember.Route.extend({
   model() {
     return this.store.findAll('question');
   },
+  actions: {
+  	deleteQuestion(qusetion) {
+      if(confirm("Are you sure you want to delete this qusetion?")) {
+        var answer_deletions = qusetion.get('answers').map(function(answer) {
+          return answer.destroyRecord();
+        });
+        Ember.RSVP.all(answer_deletions).then(function() {
+          return qusetion.destroyRecord();
+        });
+      }
+    }
+  } 
 });
