f2c55b2ea6fe3d2d2c5d54ab99361a8fceca3bd2
Adam Gorbahn <adamgorbahn@hotmail.com>
2016-10-28 08:56:25 -0700

    save of the question working

--- app/components/question-add.js
+++ app/components/question-add.js
@@ -1,4 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    save() {
+      var params = {
+        question: this.get('question') ? this.get('question') : "Please update this!",
+        author: this.get('author') ? this.get('author') : "Please update this!",
+        moreinfo: this.get('moreinfo') ? this.get('moreinfo') : "Please update this!",
+      };
+      this.sendAction('save', params);
+    }
+  },
+  beforeModel: function(){
+    this._super(...arguments);
+    if (typeof tinymce === 'undefined'){
+      return Ember.$.getScript('//cdn.tinymce.com/4/tinymce.min.js');
+    }
+  }
 });
