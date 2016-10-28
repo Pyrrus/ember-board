1fbfa17aee4805c2021b3e47f2d9b35b1a2234bf
Adam Gorbahn <adamgorbahn@hotmail.com>
2016-10-28 09:36:47 -0700

    Add and view answer for the question.

--- app/components/answer-add.js
+++ app/components/answer-add.js
@@ -1,4 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+  	save(question) {
+  	   var params = {
+        answer: this.get('answer') ? this.get('answer') : "Please update this!",
+        author: this.get('author') ? this.get('author') : "Please update this!",
+        question: question
+      };
+      this.sendAction('save', params);
+  	}
+  },
+  beforeModel: function(){
+    this._super(...arguments);
+    if (typeof tinymce === 'undefined'){
+      return Ember.$.getScript('//cdn.tinymce.com/4/tinymce.min.js');
+    }
+  }
 });
