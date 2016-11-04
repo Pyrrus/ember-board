19740bd841b36f4a4887d933ea4e6941d4dfa6bb
Adam Gorbahn <adamgorbahn@hotmail.com>
2016-11-03 20:21:42 -0700

    add helper to change the color by the number of vote.

--- app/helpers/vote-color.js
+++ app/helpers/vote-color.js
@@ -1,7 +1,17 @@
 import Ember from 'ember';
 
-export function voteColor(params/*, hash*/) {
-  return params;
+export function voteColor(params) {
+  if (params >= 10) {
+  	return "<h2 class='great'>Answer</h2>";
+  } else if (params >= 1) {
+  	return "<h2 class='good'>Answer</h2>";
+  } else if (params == 0) {
+  	return "<h2>Answer</h2>";
+  } else if (params >= -9) {
+  	return "<h2 class='bad'>Answer</h2>";
+  } else if (params <= -10) {
+  	return "<h2 class='awful'>Answer</h2>";
+  }
 }
 
 export default Ember.Helper.helper(voteColor);
